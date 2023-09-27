88fae679ecf7f9c03c079af6a1e9bc8799d94f3a
benjicoopz <benjicoopss@gmail.com>
2023-09-27 16:54:31 -0500

    fix functions

--- app.py
+++ app.py
@@ -5,28 +5,36 @@ import copy
 player_data = copy.deepcopy(PLAYERS)
 
 
+
 def clean_data(player_data):
     cleaned = []
     for user in player_data:
         fixed = {}
+        fixed["name"] = user["name"]
         fixed["height"] = int(user["height"].split(" ")[0])
         if user["experience"] == "YES":
             fixed["experience"] = True
         else:
             fixed["experience"] = False
+        fixed["guardians"] = user["guardians"].split("and")
         cleaned.append(fixed)
     return cleaned
 
+clean_players = clean_data(player_data)
 
-def balance_teams(player_data):
+def balance_teams(clean_players):
     experienced = []
     non_experienced = []
-    for user in player_data:
-        if user["experience"] == "YES":
-            experienced.append({"name"})
+    for user in clean_players:
+        if user["experience"] == True:
+            experienced.append(user)
         else:
-            non_experienced.append({"name"})
+            non_experienced.append(user)
+    print(f"experienced: {experienced}")
+    print(f"not experienced: {non_experienced}")
     
+   
+        
     
     
     
@@ -38,5 +46,5 @@ def balance_teams(player_data):
 
 
 if __name__ == "__main__" :
-    print(balance_teams(player_data))
+    balance_teams(clean_players)
     
