cb33e250c6cf0384d37f3e176ef206a90dfb9b9a
benjicoopz <benjicoopss@gmail.com>
2023-10-03 19:04:00 -0500

    work on menus

--- app.py
+++ app.py
@@ -7,34 +7,6 @@ player_data = copy.deepcopy(PLAYERS)
 team_data = copy.deepcopy(TEAMS)
 
 
-print("BASKETBALL TEAM STATS TOOL")
-print("\n---MENU---")
-print("\nHere are your choices:")
-print("\n1) Display Team Stats")
-print("2) Quit")
-while True:
-    try:
-        option = input("\nEnter an option: ")
-        if option == "1":
-            print("1) Panthers")
-            print("2) Bandits")
-            print("3) Warriors")
-            break
-        elif option == "2":
-            sys.exit()
-        else:
-            raise Exception("Please enter 1 or 2")
-    except Exception as e:
-        print(e)
-    
-
-    
-
-
-    
-
-
-
 def clean_data(player_data):
     cleaned = []
     for user in player_data:
@@ -50,6 +22,7 @@ def clean_data(player_data):
     return cleaned
 
 
+
 def balance_teams(player_data):
     experienced = []
     non_experienced = []
@@ -59,26 +32,71 @@ def balance_teams(player_data):
             experienced.append(user)
         else:
             non_experienced.append(user)
-    
+            
     
     new_teams = []
     for team in team_data:
         team_object = {"team_name": team, "players": []}
         new_teams.append(team_object)
+    
         
     while experienced:
         for team in new_teams:
             popped = experienced.pop(0)
             team["players"].append(popped)
             
+            
     while non_experienced:
         for team in new_teams:
             popped2 = non_experienced.pop(0)
             team["players"].append(popped2)
+
+    
+            
+    
+while True:            
+    print("BASKETBALL TEAM STATS TOOL")
+    print("\n---MENU---")
+    print("\nHere are your choices:")
+    print("\n1) Display Team Stats")
+    print("2) Quit")
+    try:
+            option = input("\nEnter an option: ")
+            if option == "1":
+                print("\n1) Panthers")
+                print("2) Bandits")
+                print("3) Warriors")
+                break
+            elif option == "2":
+                sys.exit()
+            else:
+                raise Exception("Please enter 1 or 2")
+    except Exception as e:
+            print(e)
+            
+    
+
+option2 = input("\nEnter an option: ")
+if option2 == "1":
+    print(new_teams)
+    
+    
+    
+    
+            
+            
+            
+            
+            
+            
+            
+            
+            
             
             
 if __name__ == "__main__" :
     clean_players = clean_data(player_data)
     balance_teams(clean_players)
+    
     
     
